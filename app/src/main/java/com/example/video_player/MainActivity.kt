32e7fd105177745d2aca8c56cb21a063f455a032
xickyy <ricky_moore@ymail.com>
2024-02-14 20:49:16 -0800

    video player should be working but is not

--- app/src/main/java/com/example/video_player/MainActivity.kt
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.video_player
 
+import android.net.Uri
+import android.net.Uri.parse
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.video_player.databinding.ActivityMainBinding
+import android.widget.MediaController
+import android.widget.VideoView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +27,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setContentView(R.layout.activity_main)
+
+//Find the VideoView class by its id
+
+        val videoView = findViewById<VideoView>(binding.testView.id)
+
+//Creating MediaController
+
+        val mediaController = MediaController(this)
+
+        mediaController.setAnchorView(videoView)
+
+//specify the location of media file
+
+        val uri: Uri = parse(
+
+            "android.resource://" + packageName
+
+                    + "/raw/test"
+
+        )
+
+//Setting MediaController and URI, then starting the videoView
+
+        videoView.setMediaController(mediaController)
+
+        videoView.setVideoURI(uri)
+
+        videoView.requestFocus()
+
+        videoView.start()
+        // specify the location of the video file
+//        val uri: Uri = parse(uriString: "android.resource://" + packageName + "/" + "test")
+//        val videoView = findById<VideoView>(binding.testView.id)
+
+
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -33,6 +74,7 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
